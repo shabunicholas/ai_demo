11e98667f26dc5d590acefa5f7eb42103cf80040
shabunicholas <2889565370@qq.com>
2026-09-11 21:16:00 +0800

    demo

--- deeptest.py
+++ deeptest.py
@@ -59,7 +59,7 @@ async def ask(req: Q):        # ① 接收前端数据
         
 
 if __name__ == "__main__":
-   uvicorn.run("deeptest:app",host="127.0.0.1",port=7000,reload=True)
+   uvicorn deeptest:app --host 0.0.0.0 --port $PORT
     
 
 
